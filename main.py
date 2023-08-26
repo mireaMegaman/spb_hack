4c8d9639fc4d18437323eb8ba5180a975339487e
Vlad <ConqurerWinner@yandex.ru>
2023-08-27 00:13:55 +0300

    add ml part

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from poolings import *
+from sentence_transformers import SentenceTransformer
+from infer import encode_one_record, predict
+from prepare_text import clean_text
 import torch
-import torch.nn as nn
-import random
 import numpy as np
 import os
+
+import warnings
+warnings.filterwarnings('ignore')
 
 
 def seed_everything(seed):
@@ -17,4 +20,19 @@ def seed_everything(seed):
 
 
 if __name__ == '__main__':
-    pass
+    seed_everything(seed=42)
+
+    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+    user_address = str(input()) # user input
+    clean_data = clean_text(user_address)
+    embedding = encode_one_record(clean_data, model)
+
+    label, place, chance = predict(embedding)
+    print(f'Predicted label is {label}, address is {place}, confidience: {chance}')
+
+
+
+
+
+
